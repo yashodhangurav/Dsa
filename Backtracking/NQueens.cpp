859ccc545ed21351bc6e55a0e670a42deebdc48d
yashodhangurav <guravyashodhan@gmail.com>
2026-01-18 12:09:28 +0530

    initial commit

---
+++ Backtracking/NQueens.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void printBoard(vector<vector<char>> board){
+    int n = board.size();
+
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            cout<<board[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<"--------------------"<<endl;
+}
+
+
+bool isSafe(vector<vector<char>> board, int row, int col){
+    int n = board.size();
+    // horizontal safety
+    for(int j = 0; j<n; j++){
+        if(board[row][j] == 'Q'){           //if any queen is present in the same row
+            return false;
+        }
+    }
+    // vertical safety
+    for(int i = 0; i<row; i++){
+        if(board[i][col] == 'Q'){
+            return false;                   //if any queen is present in the same column
+        }
+    }
+
+    // diagonal left
+    for(int i = row, j = col; i>=0 && j>=0; i--, j--){
+        if(board[i][j] == 'Q'){
+            return false;
+        }
+    }
+    // diagonal right
+    for(int i = row, j = col; i>=0 && j<n; i--, j++){
+        if(board[i][j] == 'Q'){
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
+void nQueens(vector<vector<char>> board, int row){
+    int n = board.size();
+    if(row == n){
+        printBoard(board);
+        return;
+    }
+
+    for(int j =0; j<n; j++){    //cols
+        if(isSafe(board, row, j)){
+            board[row][j] = 'Q';
+            nQueens(board, row+1);
+            board[row][j] = '.';    //remove queen (backtrack)
+        }
+    }
+}
+
+int main(){
+    vector<vector<char>> board;
+    int n = 4;
+
+    for(int i = 0; i<n; i++){
+        vector<char> newRow;
+        for(int j = 0; j<n; j++){
+            newRow.push_back('.');
+        }
+        board.push_back(newRow);
+    }
+
+    nQueens(board, 0);
+    return 0;
+}
---
+++ Backtracking/tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+
+// void nQueens(vector<vector<char>> board, int row){
+//     //in 0th row we have 2 choices
+//     int n = board.size();
+
+//     // base case
+//     if(row == n){
+//         printBoard(board);
+//         return;
+//     }
