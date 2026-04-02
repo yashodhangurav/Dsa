6f225ba1dd7efdb90884850ad630926671e30755
yashodhangurav <guravyashodhan@gmail.com>
2026-04-02 23:27:40 +0530

    initial commit

--- PRACTICE/selectionSort.cpp
+++ PRACTICE/selectionSort.cpp
@@ -1,17 +1,52 @@
+// #include<iostream>
+// using namespace std;
+
+// void selectionSort(int *arr, int n){
+
+//    for(int i = 0; i<n-1; i++){
+//     int minIdx = i;
+//     for(int j = i+1; j<n; j++){
+//         if(arr[j]<arr[minIdx]){
+//             minIdx = j;
+//         }
+//         swap(arr[i], arr[minIdx]);
+//     }
+//    }
+// }
+
+// void printArr(int *arr, int n){
+//     for(int i =0; i<n; i++){
+//         cout<<arr[i]<<", ";
+//     }
+// }
+
+// int main(){
+//     int arr[5] = {1,7,2,5,3};
+//     selectionSort(arr, 5);
+//     printArr(arr,5);
+
+//     return 0;
+// }
+
+
+// ------------------------------------------------------------
+
+
 #include<iostream>
 using namespace std;
 
+
 void selectionSort(int *arr, int n){
 
-   for(int i = 0; i<n-1; i++){
-    int minIdx = i;
-    for(int j = i+1; j<n; j++){
-        if(arr[j]<arr[minIdx]){
-            minIdx = j;
+    for(int i = 0; i<n-1; i++){
+        int minVal =i;
+        for(int j = i+1; j<n; j++){
+            if(arr[j]<arr[minVal]){
+                minVal = j;
+            }
         }
-        swap(arr[i], arr[minIdx]);
+        swap(arr[i], arr[minVal]);
     }
-   }
 }
 
 void printArr(int *arr, int n){
@@ -21,9 +56,10 @@ void printArr(int *arr, int n){
 }
 
 int main(){
-    int arr[5] = {1,7,2,5,3};
-    selectionSort(arr, 5);
-    printArr(arr,5);
+    int arr[] = {1,7,2,5,3};
+    int n = sizeof(arr)/sizeof(int);
 
+    selectionSort(arr, n);
+    printArr(arr, n);
     return 0;
 }
