e36b6ac3893f37bcfcc4a31bffcd6ddb329d94c2
yashodhangurav <guravyashodhan@gmail.com>
2026-04-08 20:34:01 +0530

    initial commit

--- BinaryTrees part 1/buildingPreorder.cpp
+++ BinaryTrees part 1/buildingPreorder.cpp
@@ -34,12 +34,47 @@ Node* buildTree(vector<int> nodes){
     return currNode;                                       //after we have built the left and right subtrees, we will return the current node
 };
 
+
+void preorder(Node* root){              //preorder traversal = ROOT -> LEFT -> RIGHT
+    if(root == NULL){
+        return;
+    }
+    cout<<root->data<<", ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+
+void inorder(Node* root){
+    if(root == NULL){
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data<<", ";
+    inorder(root->right);
+}
+
+void postorder(Node* root){
+    if(root == NULL){
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<", ";
+}
+
 int main(){
     vector<int> nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
 
     Node* root  = buildTree(nodes); //1
 
-    cout<<"ROOT = "<<root->data<<endl; 
+    // cout<<"ROOT = "<<root->data<<endl; 
 
+    cout<<"\n Preorder : ";
+    preorder(root); //Time complexity = O(n) where n is the number of nodes in the tree
+    cout<<"\n Inorder : ";
+    inorder(root);  //Time complexity = O(n) where n is the number of nodes in the tree
+    cout<<"\n Postorder : ";
+    postorder(root); //Time complexity = O(n) where n is the number of nodes in the tree
     return 0;
 }
