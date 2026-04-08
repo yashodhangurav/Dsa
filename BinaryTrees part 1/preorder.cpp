ec31d601a371e79477c04d345342cd29931a6579
yashodhangurav <guravyashodhan@gmail.com>
2026-04-08 20:11:04 +0530

    initial commit

---
+++ BinaryTrees part 1/preorder.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// Time complexity = O(n) where n is the number of nodes in the tree
+
+class Node{
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int data){
+            this->data = data;
+            this->left = NULL;
+            this->right = NULL;
+        }
+};
+
+static int idx = -1;
+
+Node* buildTree(vector<int> nodes){
+    idx++;
+    if(nodes[idx] == -1){
+        return NULL;
+    }
+
+    // creating new nodes for PREORDER  = ROOT -> LEFT -> RIGHT
+    Node* currNode = new Node(nodes[idx]);
+    // recursively building left and right subtrees
+    currNode->left = buildTree(nodes);                      //until we encounter -1, we will keep on building the left subtree
+    currNode->right = buildTree(nodes);                     //after we encounter -1, we will start building the right subtree
+
+    return currNode;                                       //after we have built the left and right subtrees, we will return the current node
+};
+
+int main(){
+    vector<int> nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+
+    Node* root  = buildTree(nodes); //1
+
+    cout<<"ROOT = "<<root->data<<endl; 
+
+    return 0;
+}
