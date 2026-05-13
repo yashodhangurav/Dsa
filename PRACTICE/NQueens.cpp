6cf34824a9879b100ad7ddb13621aab6225ab6a0
yashodhangurav <guravyashodhan@gmail.com>
2026-05-13 22:52:22 +0530

    initial commit

---
+++ PRACTICE/NQueens.cpp
@@ -0,0 +1,142 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+void printBoard(vector<vector<char>> board){
+    int n = board.size();
+
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            cout<<board[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+   
+    cout<<"--------------"<<endl;
+}
+
+bool isSafe(vector<vector<char>> board, int row, int col){
+   int n = board.size();
+   
+//  horizontal
+    for(int j = 0; j<n; j++){
+        if(board[row][j] == 'Q'){
+            return false;
+        }
+    }
+
+//  vertical
+    for(int i = 0; i<row; i++){
+        if(board[i][col] == 'Q'){
+            return false;
+        }
+    }
+
+//  upper left diagonal
+    for(int i = row, j = col; i>=0 && j>=0; i--, j--){
+        if(board[i][j] == 'Q'){
+            return false;
+        }
+    }
+
+//  upper right diagonal
+    for(int i = row, j= col; i>=0 && j<n;i--, j++){
+        if(board[i][j] == 'Q'){
+            return false;
+        }
+    }
+    return true;
+}
+
+void nQueens(vector<vector<char>> board, int row){
+    int n = board.size();
+
+    if(row == n){
+        printBoard(board);
+        cout<<endl;
+        return;
+    }
+
+    for(int j = 0; j<n; j++){
+        if(isSafe(board, row, j)){
+            board[row][j] = 'Q';
+            nQueens(board, row+1);
+            board[row][j] = '.';
+        } 
+    }
+}
+
+int main(){
+    vector<vector<char>> board;
+    int n = 4;
+
+    for(int i=0; i<n; i++){
+        vector<char> newRow;
+        for(int j = 0; j<n; j++){
+            newRow.push_back('.');
+        }
+        board.push_back(newRow);
+    }
+
+    printBoard(board);
+    nQueens(board, 0);
+
+    return 0;
+}
+
+
+
+
+
+// -----------------------------------------LEETCODE SOL
+
+
+// class Solution {
+//     public:
+//         vector<vector<string>> result;
+    
+//         bool isSafe(vector<string>& board, int row, int col) {
+//             int n = board.size();
+    
+//             // vertical
+//             for (int i = 0; i < row; i++) {
+//                 if (board[i][col] == 'Q') return false;
+//             }
+    
+//             // upper-left diagonal
+//             for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+//                 if (board[i][j] == 'Q') return false;
+//             }
+    
+//             // upper-right diagonal
+//             for (int i = row, j = col; i >= 0 && j < n; i--, j++) {
+//                 if (board[i][j] == 'Q') return false;
+//             }
+    
+//             return true;
+//         }
+    
+//         void nQueens(vector<string>& board, int row) {
+//             int n = board.size();
+    
+//             if (row == n) {
+//                 result.push_back(board);
+//                 return;
+//             }
+    
+//             for (int j = 0; j < n; j++) {
+//                 if (isSafe(board, row, j)) {
+//                     board[row][j] = 'Q';
+//                     nQueens(board, row + 1);
+//                     board[row][j] = '.'; // backtrack
+//                 }
+//             }
+//         }
+    
+//         vector<vector<string>> solveNQueens(int n) {
+//             vector<string> board(n, string(n, '.'));
+//             nQueens(board, 0);
+//             return result;
+//         }
+//     };
