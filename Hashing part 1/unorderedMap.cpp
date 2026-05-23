a02f0fa25f1f9d5794e72e4a4925a2e9db045272
yashodhangurav <guravyashodhan@gmail.com>
2026-05-23 22:32:19 +0530

    initial commit

---
+++ Hashing part 1/unorderedMap.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+
+int main(){
+    unordered_map<string, int> m;
+
+    m["india"] = 150;
+    m["china"] = 200;
+    m["US"] = 50;
+    m["UK"] = 30;
+
+
+    for(pair<string, int> country : m){
+        cout<<country.first<<", "<<country.second<<endl;
+    }
+cout<<"--------------------------------------"<<endl;
+
+    // earase func
+    m.erase("UK");
+
+     for(pair<string, int> country : m){
+        cout<<country.first<<", "<<country.second<<endl;
+    }
+
+    // count func
+    if(m["nepal"]){
+        cout<<"nepal is present"<<endl;
+    }else{
+        cout<<"nepal is not present"<<endl;
+    }
+
+    return 0;
+}
