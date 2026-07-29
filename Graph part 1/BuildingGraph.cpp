603fe18be925cd97d03aaceb701df1c6b2dd88eb
yashodhangurav <guravyashodhan@gmail.com>
2026-07-29 23:16:14 +0530

    bfs

--- Graph part 1/BuildingGraph.cpp
+++ Graph part 1/BuildingGraph.cpp
@@ -1,3 +1,56 @@
+// #include<iostream>
+// #include<vector>
+// #include<list>
+// using namespace std;
+
+// class Graph{
+//     int V;              //number of vertices
+//     list<int> * l;       //l will store the different list's of integers (just like dinamic array int * arr)
+
+//     public:
+//     Graph(int V){
+//         this->V = V;
+//         l = new list<int> [V];          // new list of size of V , just like (arr = new int[V])
+//     }
+
+//     void addEdge(int u, int v){
+//         l[u].push_back(v);
+//         l[v].push_back(u);
+//     }
+
+//     void print(){
+//         for(int u = 0; u<V; u++){
+//             list<int> neighbors = l[u];
+//             cout<<u<<" : "; 
+//             for(int v : neighbors){
+//                 cout<< v <<", ";
+//             }
+//             cout<<endl;
+//         }
+//     }
+// };
+
+
+// int main(){
+
+//     Graph graph(5);
+
+//     graph.addEdge(0,1);
+//     graph.addEdge(1,2);
+//     graph.addEdge(2,3);
+//     graph.addEdge(3,1);
+//     graph.addEdge(2,4);
+
+//     graph.print();
+
+//     return 0;
+// }
+
+
+
+// -------------------------for weighted graph
+
+
 #include<iostream>
 #include<vector>
 #include<list>
@@ -5,25 +58,25 @@ using namespace std;
 
 class Graph{
     int V;              //number of vertices
-    list<int> * l;       //l will store the different list's of integers (just like dinamic array int * arr)
+    list<pair<int,int>> * l;       //l will store the different list's of integers (just like dinamic array int * arr)
 
     public:
     Graph(int V){
         this->V = V;
-        l = new list<int> [V];          // new list of size of V , just like (arr = new int[V])
+        l = new list<pair<int,int>> [V];          // new list of size of V , just like (arr = new int[V])
     }
 
-    void addEdge(int u, int v){
-        l[u].push_back(v);
-        l[v].push_back(u);
+    void addEdge(int u, int v, int weight){
+        l[u].push_back({v, weight});
+        l[v].push_back({u, weight});
     }
 
     void print(){
         for(int u = 0; u<V; u++){
-            list<int> neighbors = l[u];
+            list<pair<int,int>> neighbors = l[u];
             cout<<u<<" : "; 
-            for(int v : neighbors){
-                cout<< v <<", ";
+            for(auto neighbors : l[u]){
+                cout<< "( "<<neighbors.first <<", "<<neighbors.second<<" s)";
             }
             cout<<endl;
         }
@@ -35,11 +88,11 @@ int main(){
 
     Graph graph(5);
 
-    graph.addEdge(0,1);
-    graph.addEdge(1,2);
-    graph.addEdge(2,3);
-    graph.addEdge(3,1);
-    graph.addEdge(2,4);
+    graph.addEdge(0,1,5);
+    graph.addEdge(1,2,1);
+    graph.addEdge(2,3,1);
+    graph.addEdge(3,1,3);
+    graph.addEdge(2,4,2);
 
     graph.print();
     
