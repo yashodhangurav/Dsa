36b95b5711967515f342194f00c78e6a9d5c355b
yashodhangurav <guravyashodhan@gmail.com>
2026-08-24 13:02:26 +0530

    commit

--- DP part 1/fibonacci.cpp
+++ DP part 1/fibonacci.cpp
@@ -25,13 +25,29 @@ int fibDP(int n, vector<int> &f){             //O(n)
 }
 
 
+int fibTab(int n){
+    vector<int> fib(n+1,0);
+    fib[0] = 0;
+    fib[1] = 1;
+
+    for(int i = 2; i<=n; i++){
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+
+    return fib[n];
+}
+
+
+
 int main(){
 
-    int n = 6;
+    int n = 7;
     vector<int> f(n+1, -1);
 
     // cout<<fib(n)<<endl;
-    cout<<fibDP(n,f)<<endl;
+    // cout<<fibDP(n,f)<<endl;
+
+    cout<<fibTab(n)<<endl;
 
     return 0;
 }
