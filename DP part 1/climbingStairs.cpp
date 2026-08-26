b9a1bb54d84957460be4733d02efa4f8bf1d8991
yashodhangurav <guravyashodhan@gmail.com>
2026-08-26 15:40:40 +0530

    tabulation

--- DP part 1/climbingStairs.cpp
+++ DP part 1/climbingStairs.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 
 
-int countWaysRec(int n){
+int countWaysRec(int n){        //O(2^n)
     if(n == 0 || n == 1){
         return 1;
     }
@@ -12,11 +12,40 @@ int countWaysRec(int n){
     return countWaysRec(n-1) + countWaysRec(n-2);
 }
 
+
+int countWaysMem(int n, vector<int> &dp){
+    if(n == 0 || n == 1){
+        return 1;
+    }
+
+    if(dp[n] != -1){
+        return dp[n];
+    }
+
+    dp[n] = countWaysMem(n-1, dp) + countWaysMem(n-2, dp);
+    return dp[n];
+}
+
+int countWaysTab(int n){
+    vector<int> dp(n+1,0);
+    dp[0] = 1;
+    dp[1] = 1;
+    for(int i = 2; i<=n; i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    return dp[n];
+}
+
 int main(){
 
     int n = 5;
+    // vector<int> dp(n+1, -1);
 
-    cout<<countWaysRec(n)<<endl;
-    
+    // cout<<countWaysRec(n)<<endl;
+
+    // cout<<countWaysMem(n, dp)<<endl;
+
+    cout<<countWaysTab(n)<<endl;
+
     return 0;
 }
