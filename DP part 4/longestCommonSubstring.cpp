ec081bbf0dd83b795afa434fe48a36ae81a44091
yashodhangurav <guravyashodhan@gmail.com>
2026-09-09 20:42:29 +0530

    longestCommonSubstring

---
+++ DP part 4/longestCommonSubstring.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+int lcSubstring(string str1, string str2){
+    int n = str1.size();
+    int m = str2.size();
+
+    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+    int ans = 0;
+    for(int i = 1; i<n+1; i++){
+        for(int j = 1; j<m+1; j++){
+            if(str1[i-1] == str2[j-1]){
+                dp[i][j] = 1+dp[i-1][j-1];
+                ans = max(ans,dp[i][j]);
+            }
+            else{
+                dp[i][j] = 0;     
+            }
+           
+        }
+    }
+    return ans;
+}
+
+int main(){
+
+    string str1 = "abcde";
+    string str2 = "abcdr";
+
+    cout<<lcSubstring(str1,str2)<<endl;
+
+    return 0;
+}
