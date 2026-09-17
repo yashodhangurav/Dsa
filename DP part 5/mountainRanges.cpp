92c7229faa487b88aff90587790ad0162b010867
yashodhangurav <guravyashodhan@gmail.com>
2026-09-17 20:56:32 +0530

    mountainRanges

---
+++ DP part 5/mountainRanges.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int mountainRanges(int n){
+    vector<int> dp(n+1, 0);
+    dp[0] = dp[1] = 1;
+
+    for(int i = 2; i<n+1; i++){
+        for(int j = 0; j<i; j++){
+            dp[i] += dp[j] * dp[i-j-1];
+        }
+    }
+
+    return dp[n];
+}
+
+int main(){
+
+    int pairs = 3;
+
+    cout <<mountainRanges(pairs)<<endl;
+    return 0;
+}
